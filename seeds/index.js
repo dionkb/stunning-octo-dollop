b0588b805efa7545bf5bd8ea0b7f214aac3433de
dionkb <dionkbaskara@gmail.com>
2023-05-19 10:28:29 -0400

    see comment, large initial commit

---
+++ seeds/index.js
@@ -0,0 +1,7 @@
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  // Fill in
+};
+
+seedAll();
---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Keeping an example helper from prev classwork in case I need something to work off of
+  
+  // format_time: (date) => {
+  //   return date.toLocaleTimeString();
+  // },
+};
