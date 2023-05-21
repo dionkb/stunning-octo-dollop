54d50d9b295d2d96662b0236d4e8f571d41a4e82
dionkb <dionkbaskara@gmail.com>
2023-05-21 18:10:36 -0400

    added seed data, now need to tweak GET routes

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,17 @@
 // Calling config for login to seqeulize via .env file
 const sequelize = require('../config/connection');
+const seedUsers = require('./userData');
+const seedBlogPosts = require('./blogPostData');
 
 // Code to run various seed files to seed the database
 const seedAll = async () => {
-  // Fill in
+  await sequelize.sync({ force: true });
+
+  await seedUsers();
+
+  await seedBlogPosts();
+
+  process.exit(0);
 };
 
 // Initializing seeding
