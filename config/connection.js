6caa3b36e518a7aa7a19f62fc9ca8f344ae1a309
dionkb <dionkbaskara@gmail.com>
2023-05-27 23:54:34 -0400

    tweaked env variables for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,28 @@
-// COMPLETE:
 // Initializing required packages
-const Sequelize = require('sequelize');
-require('dotenv').config();
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
 
 // Setting up info to be pulled from .env file for secure server login
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
 
 // Exporting file
-module.exports = sequelize;
+// module.exports = sequelize;
+
+// --------------------------------------------------------------------------------- //
+
+// Info for Heroku deployment via clearDB
+module.exports = {
+  HOST: "us-cdbr-east-06.cleardb.net",
+  USER: "b4e45312be1e82",
+  PASSWORD: "660dc92b",
+  DB: "heroku_8b70ae66cd5dbf3"
+};
