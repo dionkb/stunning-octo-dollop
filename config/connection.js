d57714f203f4f91bfe7918d05418922d0a393bbc
dionkb <dionkbaskara@gmail.com>
2023-05-28 00:16:35 -0400

    trying again to deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -20,9 +20,31 @@
 // --------------------------------------------------------------------------------- //
 
 // Info for Heroku deployment via clearDB
-module.exports = {
-  HOST: "us-cdbr-east-06.cleardb.net",
-  USER: "b4e45312be1e82",
-  PASSWORD: "660dc92b",
-  DB: "heroku_8b70ae66cd5dbf3"
-};
+// module.exports = {
+//   HOST: "us-cdbr-east-06.cleardb.net",
+//   USER: "b4e45312be1e82",
+//   PASSWORD: "heroku_8b70ae66cd5dbf3",
+//   DB: "heroku_8b70ae66cd5dbf3"
+// };
+
+// --------------------------------------------------------------------------------- //
+
+// TESTING ANOTHER DEPLOY PROCESS
+// Initializing required packages
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+// Setting up info to be pulled from .env file for secure server login
+const sequelize = new Sequelize(
+  "heroku_8b70ae66cd5dbf3",
+  "b4e45312be1e82",
+  "heroku_8b70ae66cd5dbf3",
+  {
+    host: 'us-cdbr-east-06.cleardb.net',
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
+
+// Exporting file
+module.exports = sequelize;
